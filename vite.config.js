af98ccabc03030c01fa8bcd05cc9918273588a42
John Bright Agbons <brightinojohnny@gmail.com>
2025-10-05 06:56:22 +0100

    set up and logics built

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        login: resolve(__dirname, "src/account/login.html"),
+        register: resolve(__dirname, "src/account/register.html"),
+        grocery: resolve(__dirname, "src/grocery/grocery.html"),
+        planner: resolve(__dirname, "src/planner/planner.html"),
+        recipes: resolve(__dirname, "src/recipes/recipes.html"),
+      },
+    },
+  },
+});
