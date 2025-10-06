c9a7542203418f37911b21293408ea837280ed5c
John Bright Agbons <brightinojohnny@gmail.com>
2025-10-07 00:27:53 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "./",
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
